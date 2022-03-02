d0c6a4fc44ba1d9a896e0b132651a1b981bb9806
BernardoModerno <bernardo.moderno@gmail.com>
2022-03-02 19:24:01 -0300

    Encriptografando senha

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,10 +1,17 @@
 import { PrismaClient, User } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 class UsersController {
     async save(user: User) {
         const prisma = new PrismaClient();
+        
         try {
-
+            const hash = bcrypt.hashSync(
+                user.password,
+                Number(process.env.BCRYPT_SALT_ROUNDS || 10)
+                
+            );
+            user.password = hash;
             return await prisma.user.create({
                 data: user,
             });
